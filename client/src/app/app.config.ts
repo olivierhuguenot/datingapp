93123243a36e6a8cb29b0108ee80d4aa61662006
olivierhuguenot <olivier.huguenot@bluewin.ch>
2026-08-23 12:22:14 +0200

    Added basic routing

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,14 +1,34 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { InitSevice } from '../core/services/init-sevice';
+import { lastValueFrom } from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes),
-    provideHttpClient()
+    provideRouter(routes, withViewTransitions()),
+    provideHttpClient(),
+    provideAppInitializer(async() => {
+      const initService = inject(InitSevice);
+
+      return new Promise<void>((resolve) => {
+        setTimeout(async() => {
+          try {
+            return lastValueFrom(initService.init())
+          } finally {
+            const splash = document.getElementById("initial-splash");
+            if(splash) {
+              splash.remove();
+            }
+            resolve()
+          }
+        }, 500)
+      })
+
+    })
   ]
 };
